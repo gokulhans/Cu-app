f553d73aec362e41709c93a37f8ac526fdcb97e8
govindhansv <govindhansv333@gmail.com>
2022-07-17 23:20:06 +0530

    latest commit

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -2,6 +2,7 @@ import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -38,8 +39,14 @@ class ListViewBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> appdata = ['App Name', 'App Version', 'Developed by','Spacial Thanks'];
-    List<String> devdata = ['Studygram', 'V 1.0.0', 'gbroz','Fayiz'];
+    List<String> appdata = [
+      'App Name',
+      'App Version',
+      'Developed by',
+      'Spacial Thanks'
+    ];
+    List<String> devdata = ['Studygram', 'V 1.0.0', 'gbroz', 'Fayiz'];
+    List<String> linkdata = ['https://play.google.com/store/apps/details?id=com.gbroz.studygram', '', 'https://www.instagram.com/gbrozdev/', 'https://www.instagram.com/fe_y_z_/'];
 
     return Scaffold(
       body: Column(
@@ -54,12 +61,23 @@ class ListViewBuilder extends StatelessWidget {
                   return ListTile(
                       contentPadding:
                           const EdgeInsets.only(left: 50, right: 50),
-                      trailing: Text(
-                        devdata[i],
-                        style: const TextStyle(
-                            color: Colors.green,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w800),
+                      trailing: TextButton(
+                        onPressed: () async {
+                          var url = linkdata[i];
+                          if (await canLaunchUrl(Uri.parse(url))) {
+                            await launchUrl(Uri.parse(url),
+                                mode: LaunchMode.externalNonBrowserApplication);
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        },
+                        child: Text(
+                          devdata[i],
+                          style: const TextStyle(
+                              color: Colors.green,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w800),
+                        ),
                       ),
                       title: Text(appdata[i],
                           style: const TextStyle(
